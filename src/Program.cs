ea3cb61b36da5330362c08aa95287b1c4b4b0d70
Tomáš Valenta <TomiukQT@gmail.com>
2021-11-30 18:07:19 +0100

    create new project

---
+++ src/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AdventOfCode
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello");
+            Console.ReadKey();
+        }
+    }
+}
