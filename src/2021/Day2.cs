53d285efbb360e81f41abb89576c33e23e9499d7
Tomáš Valenta <TomiukQT@gmail.com>
2021-12-02 06:10:32 +0100

    day 2

--- src/2021/Day2.cs
+++ src/2021/Day2.cs
@@ -1,3 +1,4 @@
+using System;
 using AdventOfCode.Utilities;
 
 namespace AdventOfCode._2021
@@ -5,23 +6,56 @@ namespace AdventOfCode._2021
     public class Day2 : BaseDay
     {
 
-        public Day2() : base(2, 2020)
+        public Day2() : base(2, 2021)
         {
             
         }
 
         public override string SolvePartOne()
         {
-            int output = 0;
-
-            return $"{output}";
+            int hor=0,dep = 0;
+            var input = Input.Read2D<string>(' ');
+            foreach (var line in input)
+            {
+                int val = Convert.ToInt32(line[1]);
+                if (line[0] == "forward")
+                {
+                    hor += val;
+                }
+                else if (line[0] == "up")
+                {
+                    dep -= val;
+                }
+                else
+                {
+                    dep += val;
+                }
+            }
+            return $"{dep*hor}";
         }
 
         public override string SolvePartTwo()
         {
-            int output = 0;
-
-            return $"{output}";
+            int hor=0,dep = 0,aim = 0;
+            var input = Input.Read2D<string>(' ');
+            foreach (var line in input)
+            {
+                int val = Convert.ToInt32(line[1]);
+                if (line[0] == "forward")
+                {
+                    hor += val;
+                    dep += aim * val;
+                }
+                else if (line[0] == "up")
+                {
+                    aim -= val;
+                }
+                else
+                {
+                    aim += val;
+                }
+            }
+            return $"{dep*hor}";
         }
     }
 }
