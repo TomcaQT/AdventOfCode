6985ba4f66fd92652446591a83f515d53c0a3357
Tomáš Valenta <TomiukQT@gmail.com>
2021-12-02 06:10:24 +0100

    day 1

--- src/2021/Day1.cs
+++ src/2021/Day1.cs
@@ -5,7 +5,7 @@ namespace AdventOfCode._2021
     public class Day1 : BaseDay
     {
 
-        public Day1() : base(1, 2020)
+        public Day1() : base(1, 2021)
         {
             
         }
@@ -14,6 +14,13 @@ namespace AdventOfCode._2021
         {
             int output = 0;
             var input = Input.Read<int>();
+            int prev = input[0];
+            for (int i = 1; i < input.Count; i++)
+            {
+                if (input[i] > prev)
+                    output++;
+                prev = input[i];
+            }
             return $"{output}";
         }
 
@@ -21,7 +28,13 @@ namespace AdventOfCode._2021
         {
             int output = 0;
             var input = Input.Read<int>();
-
+            int prev = input[0] + input[1] +  input[2];
+            for (int i = 3; i < input.Count; i++)
+            {
+                if ((prev - input[i-3] + input[i]) > prev)
+                    output++;
+                prev = prev - input[i-3] + input[i];
+            }
             return $"{output}";
         }
     }
