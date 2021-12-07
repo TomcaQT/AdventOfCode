b47fea1103a79b1c9089fb60dea03d9dab93ac34
Tomáš Valenta <TomiukQT@gmail.com>
2021-12-07 11:40:07 +0100

    Day8 prep

---
+++ src/2021/Day8.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Utilities;
+
+namespace AdventOfCode._2021
+{
+    public class Day8 : BaseDay
+    {
+
+        public Day8() : base(8, 2021)
+        {
+            
+        }
+        
+        public override string SolvePartOne()
+        {
+            int output = 0;
+            //int avg = (int)Math.Round(input.Average());
+            return $"{output}";
+        }
+
+        public override string SolvePartTwo()
+        {
+            int output = 0;
+
+            
+            return $"{output}";
+        }
+    }
+}
