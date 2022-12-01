9c624ae2611a67a6d849ce50450492dcaa323e11
Tomáš Valenta <TomiukQT@gmail.com>
2022-12-01 08:53:42 +0100

    feat: Update template

--- 2022/template.py
+++ 2022/template.py
@@ -11,12 +11,16 @@ def solve_part_2(data):
     pass
 
 
-def parse_raw_input(raw_data):
-    data = raw_data
+def parse_raw_input(infile):
+    lines, raw_data, data = [], None, None
+    # with open(infile) as file:
+    # lines = file.readlines()
+    with open(infile, 'r') as f:
+        raw_data = f.read().split('\n\n')
     return data
 
 
-def solve(raw_data: []):
-    data = parse_raw_input(raw_data)
+def solve(infile):
+    data = parse_raw_input(infile)
     print(solve_part_1(data))
     print(solve_part_2(data))
