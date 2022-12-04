edfd1719b1f3e8c21e7f4da1f503d31b8b63a4b1
Tomáš Valenta <TomiukQT@gmail.com>
2022-12-04 10:26:26 +0100

    day5 prep

--- 2022/main.py
+++ 2022/main.py
@@ -1,11 +1,11 @@
 import datetime
 import os
-import day4
+import day5
 
 
 def main():
     day = str(datetime.date.today().day)
-    day4.solve(f'inputs/{day}')
+    day5.solve(f'inputs/{day}')
 
 
 if __name__ == '__main__':
