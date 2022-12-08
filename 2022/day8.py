9d9e570ee94599696b953846654ca1ee3a971384
Tomáš Valenta <TomiukQT@gmail.com>
2022-12-08 12:53:05 +0100

    day8 done

--- 2022/day8.py
+++ 2022/day8.py
@@ -3,20 +3,53 @@ import numpy as np
 import pandas as pd
 
 
+def check_vis(data, t):
+    return all(get_vis(data, t))
+
+
+def get_vis(data, t, rev=False):
+    return list(d < t for d in data) if not rev else list(reversed(list(d < t for d in data)))
+
+
 def solve_part_1(data):
-    pass
+    res = 0
+    for y, l in enumerate(data):
+        for x, t in enumerate(l):
+            if check_vis(data[y][0:x], t) or check_vis(data[y][x+1:], t) or \
+                    check_vis(column(data, x)[0:y], t) or check_vis(column(data, x)[y+1:], t):
+                res += 1
+    return res
+
+
+def get_vis_count(x):
+    return x.index(False)+1 if False in x else len(x)
 
 
 def solve_part_2(data):
-    pass
+    res = 0
+    for y, _l in enumerate(data):
+        for x, t in enumerate(_l):
+            l, r = get_vis(data[y][0:x], t, True), get_vis(data[y][x + 1:], t)
+            u, d = get_vis(column(data, x)[0:y], t, True), get_vis(column(data, x)[y + 1:], t)
+            tmp = get_vis_count(l) * get_vis_count(r) * get_vis_count(u) * get_vis_count(d)
+            res = max(tmp, res)
+    return res
+
+
+def column(matrix, i):
+    return [row[i] for row in matrix]
 
 
 def parse_raw_input(infile):
-    lines, raw_data, data = [], None, None
+    lines, raw_data, data = [], None, []
     # with open(infile) as file:
     # lines = file.readlines()
     with open(infile, 'r') as f:
-        raw_data = f.read().split('\n\n')
+        raw_data = f.read().split('\n')
+    for line in raw_data:
+        l = []
+        l.extend(list(map(int, [*line])))
+        data.append(l)
     return data
 
 
